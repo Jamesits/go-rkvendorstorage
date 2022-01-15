ec1317ba542caa2cc78f5b8abac282babaf26704
James Swineson <github@public.swineson.me>
2022-01-16 00:37:51 +0800

    update comments

--- consts.go
+++ consts.go
@@ -4,11 +4,11 @@ package rkvendorstorage
 const DevicePath = "/dev/vendor_storage"
 
 // RequestTag must appear at the first 4 bytes of the buffer.
-// Contains the string "VREQ".
+// Probably means "VREQ", but characters are reversed in the actual buffer as these chips are of little endian.
 const RequestTag = 0x56524551
 
 // RequestMaxSize is the max size of data you can read or write.
-// The value is hardcoded in many places, like [vendor_storage.c](https://github.com/rockchip-linux/rktoolkit/blob/77fb41f99185a6f9dc2c9c69e099f417d8ae905d/vendor_storage.c#L31).
+// The value is hardcoded differently in different documentations, so a reasonable default is selected.
 const RequestMaxSize = 1024
 
 // RequestBufferMaxSize is the size of the buffer used for ioctl.
@@ -22,6 +22,7 @@ const IORequestRead uintptr = 0x40047601
 // To calculate on the fly: var IORequestWrite = _IOW('v', 0x02, 4)
 const IORequestWrite uintptr = 0x40047602
 
+// These are pre-defined keys.
 const (
 	IDUnknown uint16 = iota
 	IDVendorSN
